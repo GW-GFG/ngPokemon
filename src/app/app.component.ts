2b5c7a0020d6f6005707ebdedfc4bb6ed601658e
Gwendoline Gaches <gwendoline.gaches@outlook.fr>
2024-04-30 15:18:34 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+import { POKEMONS } from './mock-pokemon-list';
+import { Pokemon } from './pokemon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   template: `
-    <h1>Welcome to {{title}}!</h1>
+    <h1>Ma mini liste de Pokémon avec Angular !</h1>
 
     <router-outlet />
   `,
   styles: [],
 })
-export class AppComponent {
-  title = 'ng-pokemon-app';
+
+export class AppComponent implements OnInit {
+  // title = 'ng-pokemon-app';
+  pokemonList: Pokemon[] = POKEMONS;
+
+  ngOnInit(): void {
+    console.table(this.pokemonList)
+    this.selectPokemon(this.pokemonList[5])
+  }
+
+  selectPokemon(pokemon: Pokemon) {
+    console.log(`Vous avez cliqué sur le pokémon ${pokemon.name}`);
+  
+  }
+
+
 }
+
