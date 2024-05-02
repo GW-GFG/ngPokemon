429798813d9c7bd365bcbc7ccba459ab9dfbbedf
Gwendoline Gaches <gwendoline.gaches@outlook.fr>
2024-05-02 20:33:34 +0200

    interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,25 +8,34 @@ import { Pokemon } from './pokemon';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  template: `
-    <h1>Ma mini liste de Pokémon avec Angular !</h1>
-
-    <router-outlet />
-  `,
+  templateUrl: './app.component.html',
   styles: [],
 })
 
 export class AppComponent implements OnInit {
   // title = 'ng-pokemon-app';
   pokemonList: Pokemon[] = POKEMONS;
+  pokemonSelected: Pokemon | undefined;
 
   ngOnInit(): void {
-    console.table(this.pokemonList)
-    this.selectPokemon(this.pokemonList[5])
+    console.table(this.pokemonList);
   }
 
-  selectPokemon(pokemon: Pokemon) {
-    console.log(`Vous avez cliqué sur le pokémon ${pokemon.name}`);
+  selectPokemon(pokemonSelectedId: string) {
+    //Here + is use to convert .value into a number (Same as Number())
+    // const index: number = +(event.target as HTMLInputElement).value;
+    let pokemonId: number = +pokemonSelectedId;
+    const pokemon: Pokemon | undefined = this.pokemonList.find(pokemon => pokemon.id == pokemonId);
+    if(pokemon) {
+      console.log(`Vous avez cliqué sur le pokémon ${pokemon.name}`);
+      this.pokemonSelected = pokemon;;
+    } else {
+      console.log(`Oups, le pokémon n'est pas reconnu !`);
+      this.pokemonSelected = pokemon;
+    }
+
+
+    
   
   }
 
